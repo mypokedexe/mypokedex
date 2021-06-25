595be15e5ae62688dff6137a78150cb5fd81d75f
Dev Front <developermater26@gmail.com>
2021-06-24 22:16:14 -0600

    se crearon las variables a usar en el poryecto

---
+++ app-js/write_pokemons.js
@@ -0,0 +1,18 @@
+export const WritePokemons = (dataPokemon )=>{
+    let {imagePokemon} = dataPokemon
+    
+    // CONSTANT CREATE
+    const $main = document.querySelector('main')
+    const Fragment = document.createDocumentFragment()
+    const $pokemonBox = document.createElement('section')
+    const $pokemonImage = document.createElement('img')
+    const $pokemon = document.createElement('section')
+    const $pokemonNameBox = document.createElement('article')
+    const $pokemonName = document.createElement('p')
+    const $pokemonDescription = document.createElement('article')
+    const $pokemonDescriptionNumberBox = document.createElement('div')
+    const $pokemonDescriptionNumber = document.createElement('p')
+    const $pokemonDescriptionTypeBox = document.createElement('div')
+    const $pokemonDescriptionType = document.createElement('p')
+    const $pokemonClick = document.createElement('section')
+}
