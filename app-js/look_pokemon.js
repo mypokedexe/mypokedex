7fb207bd5d552353a97e2ccfb8424de809994aa2
Dev Front <developermater26@gmail.com>
2021-06-25 18:23:52 -0600

    se crearon las variables a usar en el poyecto y se destructuro un arreglo que nos dara toda la data para poder mostrar el pokemon

---
+++ app-js/look_pokemon.js
@@ -0,0 +1,15 @@
+export const LookPokemon = (SkillsDescription )=>{
+    let {Altura,Categoria,Peso,Habilidad,Sexo,Tipo} = SkillsDescription
+    const body = document.querySelector('body') 
+    const Fragment = document.createDocumentFragment()
+    const $pokemonBoxActive = document.createElement('section')
+    const $arrowActive = document.createElement('i')
+    const $pokemonImage = document.createElement('img')
+    const $pokemonImageShadow = document.createElement('section')
+    const $pokemonActive = document.createElement('section')
+    const $pokemonNameBoxActive = document.createElement('article')
+    const $pokemonNameActive = document.createElement('p')
+    const $pokemonDescriptionActive = document.createElement('article')
+    const $pokemonListDescription = document.createElement('ul')
+
+}
