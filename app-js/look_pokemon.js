c004a367eb581aac623ae7eea02a2cdfe8c848ad
Dev Front <developermater26@gmail.com>
2021-06-25 18:26:05 -0600

    se creo un arreglo y ser recorrio para poder crear los li que contienen todos los datos del pokemon

--- app-js/look_pokemon.js
+++ app-js/look_pokemon.js
@@ -11,5 +11,55 @@ export const LookPokemon = (SkillsDescription )=>{
     const $pokemonNameActive = document.createElement('p')
     const $pokemonDescriptionActive = document.createElement('article')
     const $pokemonListDescription = document.createElement('ul')
+    const SkillsName = [
+        'Altura',
+        'Categoría',
+        'Peso',
+        'Habilidad',
+        'Sexo',
+        '',
+        'Tipo'
+    ]
 
+    SkillsName.forEach(el => {
+        const $pokemonListDescriptionItem = document.createElement('li')
+        const $pokemonListDescriptionItemTitle = document.createElement('p')
+        const $pokemonListDescriptionItemDescription  = document.createElement('p')
+        $pokemonListDescriptionItemTitle.textContent = el
+        $pokemonListDescriptionItem.classList.add('pokemon__list__description__item')
+        $pokemonListDescriptionItemTitle.classList.add('pokemon__list__desctiption__item__title')
+        $pokemonListDescriptionItemDescription.classList.add('pokemon__list__desctiption__item__description')
+
+        switch (el) {
+            case 'Altura':
+                $pokemonListDescriptionItemDescription.textContent = Altura
+                break;
+                
+            case 'Categoría':
+                $pokemonListDescriptionItemDescription.textContent = Categoria
+            break;
+
+            case 'Peso':
+                $pokemonListDescriptionItemDescription.textContent = Peso
+            break;
+            
+            case 'Habilidad':
+                $pokemonListDescriptionItemDescription.textContent = Habilidad
+            break;
+
+            case 'Sexo':
+                $pokemonListDescriptionItemDescription.textContent = Sexo
+            break;
+        
+            default:
+            break;
+        }
+        
+        $pokemonListDescriptionItem.appendChild($pokemonListDescriptionItemTitle)
+        $pokemonListDescriptionItem.appendChild($pokemonListDescriptionItemDescription)
+        $pokemonListDescription.appendChild($pokemonListDescriptionItem)
+    });
+
+    $pokemonImage.src = 'https://raw.githubusercontent.com/PokeMiners/pogo_assets/master/Images/Pokemon/pokemon_icon_006_00.png'
+    $pokemonNameActive.textContent = 'Charmander'
 }
