8f7b13e2759a05b2fdf95c75aa612bcc29a4b6cc
Dev Front <developermater26@gmail.com>
2021-06-28 20:54:33 -0600

    se crearon las variables a utilizar y una imortacion de modulo

--- app-js/index_module.js
+++ app-js/index_module.js
@@ -1,7 +1,5 @@
-
-
-
-
-document.addEventListener('DOMContentLoaded',(e)=>{
-
-})
+import { LookPokemon } from "./look_pokemon.js";
+let $next  = document.querySelector('#arrow-rigth')
+let $previous  = document.querySelector('#arrow-left')
+let endpoinOne = 'https://pokeapi.co/api/v2/pokemon/'
+let $main = document.querySelector('main')
