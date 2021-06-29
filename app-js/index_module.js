67f3dcd90d1757973a9f084d8e004de45a136071
Dev Front <developermater26@gmail.com>
2021-06-28 21:04:16 -0600

    se recorrio el results del json para poder hacer la otra llamada a el endpoin de los pokemons para obtener sus especificaciones

--- app-js/index_module.js
+++ app-js/index_module.js
@@ -9,6 +9,47 @@ const GetAllPokemons = async (url)=>{
         let res = await fetch(url)
         let json =await res.json()
 
+        if(!res.ok) throw res
+        
+        let $template = ''
+
+        for (let i = 0; i < json.results.length; i++) {
+            const element = json.results[i];
+            try {
+                let res = await fetch(element.url)                
+                let pokemon = await res.json()
+              
+                $template += `<section class="pokemon__box">
+                                <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}" class="pokemon__image">
+                                <section class="pokemon">
+                                    <article class="pokemon__name__box">
+                                        <p class="pokemon__name">${pokemon.name}</p>
+                                    </article>
+
+                                    <article class="pokemon__description">
+                                        <div class="pokemon__desctipon__number__box">
+                                            <p class="pokemon__description__number">${pokemon.id}</p>
+                                        </div>
+
+                                        <div class="pokemon__description__type__box">
+                                            <p class="pokemon__description__type">${pokemon.types[0].type.name}</p>
+                                        </div>
+                                    </article>
+
+                                </section>
+
+                                <section class="pokemon__click" data-pokemon="${pokemon.name}"></section>
+
+                            </section>
+                `
+                if(!res.ok)throw res
+            } catch (err) {
+                console.log(`${err.status} Ocurrio un Error`);
+            }
+        }
+
+
+        $main.innerHTML = $template
         let next  = json.next === null ? '' : json.next
         let previous  = json.previous === null ? '' : json.previous
         $next.dataset.link = next
