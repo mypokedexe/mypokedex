9047aec2046f9f5e1611b2e37128eab5f85d2f11
Dev Front <developermater26@gmail.com>
2021-06-28 21:01:11 -0600

    se hizo una llamada a el primer endpoin y se hizo la validacion para los botones de next y previous

--- app-js/index_module.js
+++ app-js/index_module.js
@@ -3,3 +3,17 @@ let $next  = document.querySelector('#arrow-rigth')
 let $previous  = document.querySelector('#arrow-left')
 let endpoinOne = 'https://pokeapi.co/api/v2/pokemon/'
 let $main = document.querySelector('main')
+const GetAllPokemons = async (url)=>{
+
+    try {
+        let res = await fetch(url)
+        let json =await res.json()
+
+        let next  = json.next === null ? '' : json.next
+        let previous  = json.previous === null ? '' : json.previous
+        $next.dataset.link = next
+        $previous.dataset.link = previous
+    } catch (err) {
+    }
+}
+document.addEventListener('DOMContentLoaded',(e)=>GetAllPokemons(endpoinOne))
