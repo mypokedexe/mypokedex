3c4776b89d57551aae85a2633acac6d264ee1f66
devFron <developermater26@gmail.com>
2021-10-07 20:41:04 -0600

    se agrego un listener con el vento click al document y se valido el target para poder cambiar el hash

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import App from "./App.js";
 document.addEventListener('DOMContentLoaded',(e)=>App())
 window.addEventListener('hashchange',(e)=>App())
+document.addEventListener('click',(e)=>{
+    if(e.target.matches('.pokemon__see')){
+        location.hash = `#/pokemon/${e.target.dataset.name}`
+    }
+})
