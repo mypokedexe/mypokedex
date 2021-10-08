98d9e9b39817aed1b54e35c79b1b7fb076e355e7
devFron <developermater26@gmail.com>
2021-10-07 20:46:13 -0600

    se hace una peticion ajax para poder recuperar los pokemones y poder usar el componente pokemons para poder pintar cards con los pokemones

---
+++ src/helpers/get_pokemons.js
@@ -0,0 +1,39 @@
+import Pokemons from "../components/Pokemons.js"
+const GetPokemons = async(url)=>{
+    try {
+        const $PokemonBox = document.createElement('section')
+        $PokemonBox.classList.add('pokemons__box')
+        let res = await fetch(url||'https://pokeapi.co/api/v2/pokemon')
+        if(!res.ok) throw res.status
+        let json = await res.json()
+        console.log(json)
+        for (let i = 0; i < json.results.length; i++) {
+            const el = json.results[i];
+            try {
+                let res = await fetch(el.url)
+                if(!res.ok) throw res.status
+                let json = await res.json()
+                $PokemonBox.appendChild(Pokemons({
+                    name:json.name,
+                    image:json.sprites.front_default,
+                    number:json.order,
+                    type:json.types[0].type.name
+                }))
+            } catch (err) {
+                console.log(err)
+                // error al recuperar los pokemons
+            }
+        }
+        let next = document.querySelector('.fa-arrow-right')
+        let previous = document.querySelector('.fa-arrow-left')
+
+        next.dataset.link = json.next !== null ?json.next : null
+        previous.dataset.link = json.previous !== null ?json.previous : null
+
+        return $PokemonBox
+    } catch (err) {
+        console.log(err)
+        // error al recuperar los pokemons
+    }
+}
+export default GetPokemons
