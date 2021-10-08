f6ac5c36fb4fa082f90bba9c99b08d8e3c58bed8
devFron <developermater26@gmail.com>
2021-10-07 20:49:35 -0600

    se importaron Header y GetPokemons despues se valida el hash para poder insetart dinamicamente cada componente

--- src/Router.js
+++ src/Router.js
@@ -1,2 +1,16 @@
-const Router =async ()=>{}
+import Header from "./components/Header.js"
+import GetPokemons from "./helpers/get_pokemons.js"
+const Router =async ()=>{
+    const $DynamicContent = document.getElementById('dynamic-content')
+    $DynamicContent.innerHTML = ''
+    let {hash}=location
+    if(hash === '#/home'|| hash === ''){
+        $DynamicContent.insertAdjacentElement('beforebegin',Header())
+        $DynamicContent.appendChild(await GetPokemons())
+    }else if(hash.includes('#/pokemon')){
+        $DynamicContent.innerHTML = `<h1 class="">Hello world</h1>`        
+    }else{
+        // erro page 404
+    }
+}
 export default Router
