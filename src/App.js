84cb58c7ad82d0bbba44afed3b379665c637420e
devFron <developermater26@gmail.com>
2021-10-07 19:46:49 -0600

    se creo un loader

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import Router from "./Router.js"
 const App = async ()=>{
     const $ROOT = document.getElementById('root')
     const $DynamicContent = document.createElement('section')
     const $loader = document.createElement('div')
-    $loader.classList.add('loader')
     $loader.innerHTML=`<img src="public/oval.svg" alt="loader" class="loader__image">`
+    $loader.classList.add('loader')
     $DynamicContent.id = 'dynamic-content' 
-    $ROOT.innerHTML = ''
     $ROOT.appendChild($DynamicContent)
-    $ROOT.appendChild($loader)
-    await Router()
-    $ROOT.removeChild($loader)
 }
 export default App
