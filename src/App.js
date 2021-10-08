cdcd6aba022368899e874fa801513a68234b6b2d
devFron <developermater26@gmail.com>
2021-10-07 19:45:00 -0600

    App se convirtio en una funcion asyncrona y se capturo el id root y se agrego un section con el ID dynamic-content y se agrega como hijo a root

--- src/App.js
+++ src/App.js
@@ -1,2 +1,15 @@
-const App = ()=>{}
+import Router from "./Router.js"
+const App = async ()=>{
+    const $ROOT = document.getElementById('root')
+    const $DynamicContent = document.createElement('section')
+    const $loader = document.createElement('div')
+    $loader.classList.add('loader')
+    $loader.innerHTML=`<img src="public/oval.svg" alt="loader" class="loader__image">`
+    $DynamicContent.id = 'dynamic-content' 
+    $ROOT.innerHTML = ''
+    $ROOT.appendChild($DynamicContent)
+    $ROOT.appendChild($loader)
+    await Router()
+    $ROOT.removeChild($loader)
+}
 export default App
