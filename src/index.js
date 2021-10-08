eaae102e4f699281322ff2bf038d49fd06cf19ba
devFron <developermater26@gmail.com>
2021-10-07 20:50:24 -0600

    se valida el target del click para poder hacer una peticion ajax para recuperar los siguientes pokemones

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import App from "./App.js";
+import GetPokemons from "./helpers/get_pokemons.js";
 document.addEventListener('DOMContentLoaded',(e)=>App())
 window.addEventListener('hashchange',(e)=>App())
-document.addEventListener('click',(e)=>{
+document.addEventListener('click',async(e)=>{
     if(e.target.matches('.pokemon__see')){
         location.hash = `#/pokemon/${e.target.dataset.name}`
+    }
+     if(e.target.matches('.pagination__link')||e.target.matches('.pagination__icon')){
+        e.preventDefault()
+        const $DynamicContent = document.getElementById('dynamic-content')
+        if(e.target.dataset.link !== 'null'){
+            $DynamicContent.innerHTML = ''
+            $DynamicContent.appendChild(await GetPokemons(e.target.dataset.link))
+        }
     }
 })
